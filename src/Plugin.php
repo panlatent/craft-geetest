510b1393e654d65c41396805878a82bb09dd6643
panlatent <panlatent@gmail.com>
2018-09-22 23:51:00 +0800

    Update version 0.2.0

--- src/Plugin.php
+++ src/Plugin.php
@@ -49,7 +49,7 @@ class Plugin extends \craft\base\Plugin
      *
      * @var string
      */
-    public $schemaVersion = '0.1.0';
+    public $schemaVersion = '0.2.0';
 
     /**
      * @var string
